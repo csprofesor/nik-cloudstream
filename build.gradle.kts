233cb6eb06c7bccad4375b7bcd78bef5ea0b01d4
profesör <gokturksahanss@gmail.com>
2026-08-22 01:41:22 +0300

    chore: Gradle yapılandırmasını güncelleyin (SDK 35, Kotlin 2.1.0, Jackson 2.17.1)

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,13 +36,13 @@ subprojects {
 
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/nikyokki/nik-cloudstream")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/csprofesor/nik-cloudstream")
 
-        authors = listOf("nikyokki")
+        authors = listOf("csprofesor", "nikyokki")
     }
 
     android {
-        namespace = "com.nikyokki"
+        namespace = "com.csprofesor"
 
         defaultConfig {
             minSdk = 21
@@ -83,9 +83,9 @@ subprojects {
         implementation(kotlin("stdlib"))                                              // Kotlin'in temel kütüphanesi
         implementation("com.github.Blatzar:NiceHttp:0.4.11")                          // HTTP kütüphanesi
         implementation("org.jsoup:jsoup:1.18.3")                                      // HTML ayrıştırıcı
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.0")   // Kotlin için Jackson JSON kütüphanesi
-        implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")          // JSON-nesne dönüştürme kütüphanesi
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")      // Kotlin için asenkron işlemler
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.1")   // Kotlin için Jackson JSON kütüphanesi
+        implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")          // JSON-nesne dönüştürme kütüphanesi
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")      // Kotlin için asenkron işlemler
 
     }
 }
