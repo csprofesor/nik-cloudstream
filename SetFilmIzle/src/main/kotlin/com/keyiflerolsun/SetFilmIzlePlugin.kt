fafb4bc506e078921eb6cb905f6e132485ede6a4
profesör <gokturksahanss@gmail.com>
2026-08-22 23:30:04 +0300

    SetFilmIzle plugin paketini geri al

--- SetFilmIzle/src/main/kotlin/com/keyiflerolsun/SetFilmIzlePlugin.kt
+++ SetFilmIzle/src/main/kotlin/com/keyiflerolsun/SetFilmIzlePlugin.kt
@@ -1,4 +1,4 @@
-package com.nikyokki
+package com.keyiflerolsun
 
 import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
@@ -7,6 +7,8 @@ import com.lagradost.cloudstream3.plugins.Plugin
 @CloudstreamPlugin
 class SetFilmIzlePlugin : Plugin() {
     override fun load(context: Context) {
+        // Önce yalnızca ana sağlayıcıyı yükle. Extractor'lar ayrı ayrı
+        // yüklenmediği için eklenti açılışında extractor kaynaklı çökme olmaz.
         registerMainAPI(SetFilmIzle())
     }
 }
