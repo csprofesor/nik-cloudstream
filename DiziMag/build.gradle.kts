e26b911551c07530f1fc13e666d9aefdd3ff4861
profesör <gokturksahanss@gmail.com>
2026-08-23 13:51:52 +0300

    DiziMag: güncel site için sürüm 21

--- DiziMag/build.gradle.kts
+++ DiziMag/build.gradle.kts
@@ -1,18 +1,10 @@
-version = 20
+version = 21
 
 cloudstream {
     authors     = listOf("nikyokki")
     language    = "tr"
     description = "En son çıkan yabancı dizi ve filmleri DiziMag farkıyla full hd 1080p kalitede izle. Dizimag Geniş ve güncel arşiviyle dizi ve filmlerin tadını çıkartın."
-
-    /**
-     * Status int as the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta only
-     **/
     status  = 1
-    tvTypes = listOf("TvSeries")
-    iconUrl = "https://www.google.com/s2/favicons?domain=dizimag.com.tr&sz=%size%"
+    tvTypes = listOf("TvSeries", "Movie")
+    iconUrl = "https://www.google.com/s2/favicons?domain=www.dizimag.life&sz=%size%"
 }
