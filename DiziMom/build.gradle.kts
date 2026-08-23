4c9bb03205c0904f5540ee4215c079535c639e33
profesör <gokturksahanss@gmail.com>
2026-08-23 17:06:57 +0300

    Set DiziMom provider author to gsrepo

--- DiziMom/build.gradle.kts
+++ DiziMom/build.gradle.kts
@@ -1,7 +1,7 @@
 version = 28
 
 cloudstream {
-    authors     = listOf("keyiflerolsun", "nikyokki")
+    authors     = listOf("gsrepo")
     language    = "tr"
     description = "Binlerce yerli yabancı dizi arşivi, tüm sezonlar, kesintisiz bölümler. Sadece dizi izle, Dizimom heryerde seninle!"
 
@@ -12,7 +12,7 @@ cloudstream {
      * 2: Slow
      * 3: Beta only
     **/
-    status  = 1 // will be 3 if unspecified
+    status  = 1
     tvTypes = listOf("TvSeries")
     iconUrl = "https://www.google.com/s2/favicons?domain=www.dizimom.surf&sz=%size%"
 }
