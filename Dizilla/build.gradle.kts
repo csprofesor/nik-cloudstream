c369ace68a40dec3080bbb73c8112e52dab575e9
profesör <gokturksahanss@gmail.com>
2026-08-23 13:04:40 +0300

    Dizilla: yeni domain için sürümü 28 yap

--- Dizilla/build.gradle.kts
+++ Dizilla/build.gradle.kts
@@ -1,18 +1,10 @@
-version = 27
+version = 28
 
 cloudstream {
     authors     = listOf("keyiflerolsun", "nikyokki")
     language    = "tr"
     description = "Dizilla tüm yabancı dizileri ücretsiz olarak Türkçe Dublaj ve altyazılı seçenekleri ile 1080P kalite izleyebileceğiniz yeni nesil yabancı dizi izleme siteniz."
-
-    /**
-     * Status int as the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta only
-    **/
-    status  = 1 // will be 3 if unspecified
+    status  = 1
     tvTypes = listOf("TvSeries")
-    iconUrl = "https://www.google.com/s2/favicons?domain=dizilla40.com&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=dizilla.now&sz=%size%"
 }
