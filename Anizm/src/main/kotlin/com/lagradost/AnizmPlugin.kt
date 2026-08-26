871ed8e11ee3afd301a9030f39ce9df3a38bef69
profesör <gokturksahanss@gmail.com>
2026-08-27 01:37:51 +0300

    Clone working Anizm plugin registration

--- Anizm/src/main/kotlin/com/lagradost/AnizmPlugin.kt
+++ Anizm/src/main/kotlin/com/lagradost/AnizmPlugin.kt
@@ -1,8 +1,8 @@
 package com.lagradost
 
-import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
 
 @CloudstreamPlugin
 class AnizmPlugin: Plugin() {
