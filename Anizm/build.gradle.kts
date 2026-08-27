0b0bac2eca2faf5d83c87bc2c5c1dc689f492961
profesör <gokturksahanss@gmail.com>
2026-08-28 00:40:11 +0300

    Replace Anizm with upstream provider files

--- Anizm/build.gradle.kts
+++ Anizm/build.gradle.kts
@@ -7,7 +7,7 @@ cloudstream {
     // All of these properties are optional, you can safely remove them
 
     // description = "Lorem Ipsum"
-     authors = listOf("csprofesor")
+     authors = listOf("Hexated")
 
     /**
      * Status int as the following:
--- Anizm/src/main/kotlin/com/lagradost/AnizmPlugin.kt
+++ Anizm/src/main/kotlin/com/lagradost/AnizmPlugin.kt
@@ -1,3 +1,4 @@
+
 package com.lagradost
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
@@ -7,6 +8,7 @@ import android.content.Context
 @CloudstreamPlugin
 class AnizmPlugin: Plugin() {
     override fun load(context: Context) {
+        // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(Anizm())
     }
 }
