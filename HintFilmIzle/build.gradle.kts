fced6d758e1abed257c303e07a398b3e3c48ab4a
profesör <gokturksahanss@gmail.com>
2026-08-28 00:51:57 +0300

    Add HintFilmIzle CloudStream extension

---
+++ HintFilmIzle/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    authors = listOf("csprofesor")
+    language = "tr"
+    description = "HintFilmizle.com Hint filmleri, yabancı filmler ve diziler"
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=www.hintfilmizle.com&sz=%size%"
+}
