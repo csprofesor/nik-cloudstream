b0dc959e506a753ab0a5b761e542a4a80a2d623a
profesör <gokturksahanss@gmail.com>
2026-08-29 01:24:07 +0300

    Match developer-provided Anizm build metadata

--- Anizm/build.gradle.kts
+++ Anizm/build.gradle.kts
@@ -1,27 +1,13 @@
-// use an integer for version numbers
-version = 3
-
+version = 5
 
 cloudstream {
     language = "tr"
-    // All of these properties are optional, you can safely remove them
-
-    // description = "Lorem Ipsum"
-     authors = listOf("csprofesor")
-
-    /**
-     * Status int as the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta only
-     * */
-    status = 1 // will be 3 if unspecified
+    authors = listOf("csprofesor")
+    status = 1
     tvTypes = listOf(
         "AnimeMovie",
         "Anime",
         "OVA",
     )
-
-    iconUrl = "https://www.google.com/s2/favicons?domain=anizm.net&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=anizm.com.tr&sz=%size%"
 }
