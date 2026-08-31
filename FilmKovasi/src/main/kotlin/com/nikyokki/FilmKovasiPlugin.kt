f059d1c9f41d7430815fe7b8891532d685810bc9
profesör <gokturksahanss@gmail.com>
2026-09-01 00:22:51 +0300

    feat: register FilmKovasi OK.ru fallback

--- FilmKovasi/src/main/kotlin/com/nikyokki/FilmKovasiPlugin.kt
+++ FilmKovasi/src/main/kotlin/com/nikyokki/FilmKovasiPlugin.kt
@@ -2,11 +2,13 @@ package com.nikyokki
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import com.nikyokki.extractors.FilmKovasiOkRuExtractor
 import android.content.Context
 
 @CloudstreamPlugin
 class FilmKovasiPlugin: Plugin() {
     override fun load(context: Context) {
+        registerExtractorAPI(FilmKovasiOkRuExtractor())
         registerMainAPI(FilmKovasi())
     }
 }
