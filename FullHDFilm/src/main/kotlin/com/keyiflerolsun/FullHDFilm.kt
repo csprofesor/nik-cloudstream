45e882b0179c014a85b187c7ea2de85c2c048c00
gokturksahan <gokturksahanss@gmail.com>
2026-09-19 20:26:29 +0300

    Update FullHDFilm to v39 with fullfilmizle.fit and Cloudflare bypass

--- FullHDFilm/src/main/kotlin/com/keyiflerolsun/FullHDFilm.kt
+++ FullHDFilm/src/main/kotlin/com/keyiflerolsun/FullHDFilm.kt
@@ -1,4 +1,4 @@
-// ! Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
+// ! Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır. (Updated for fullfilmizle.fit v39)
 
 package com.keyiflerolsun
 
