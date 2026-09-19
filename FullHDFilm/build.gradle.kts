7acd6fff36613604192a964d81cfb11a71b2b986
csprofesor <199635364+csprofesor@users.noreply.github.com>
2026-09-19 18:04:13 +0000

    Sync plugins from Feroxx [feroxx-sync]

--- FullHDFilm/build.gradle.kts
+++ FullHDFilm/build.gradle.kts
@@ -1,4 +1,4 @@
-version = 41
+version = 36
 
 cloudstream {
     authors = listOf("csprofesor")
@@ -14,5 +14,5 @@ cloudstream {
     **/
     status  = 1 // will be 3 if unspecified
     tvTypes = listOf("Movie", "TvSeries")
-    iconUrl = "https://www.google.com/s2/favicons?domain=fullfilmizle.fit&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=hdfilm.us&sz=%size%"
 }
