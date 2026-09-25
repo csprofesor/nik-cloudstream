367a7484f53a74c06dd9f234a133018383ec992c
csprofesor <199635364+csprofesor@users.noreply.github.com>
2026-09-25 20:04:16 +0000

    Sync plugins from Feroxx [feroxx-sync]

--- HDFilmCehennemi/src/main/kotlin/com/keyiflerolsun/HDFilmCehennemi.kt
+++ HDFilmCehennemi/src/main/kotlin/com/keyiflerolsun/HDFilmCehennemi.kt
@@ -2,7 +2,6 @@
 
 package com.keyiflerolsun
 
-import android.util.Base64
 import android.util.Log
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.DeserializationFeature
@@ -273,12 +272,7 @@ class HDFilmCehennemi : MainAPI() {
                         while (paddedResult.length % 4 != 0) {
                             paddedResult += "="
                         }
-                        try {
-                            result = String(Base64.decode(paddedResult, Base64.DEFAULT), Charsets.ISO_8859_1)
-                        } catch (e: Exception) {
-                            // Catch and ignore bad base64 to avoid total crash, or leave result as is
-                            Log.e("HDCH", "Failed to base64 decode: $paddedResult")
-                        }
+                        result = String(android.util.Base64.decode(paddedResult, android.util.Base64.NO_WRAP), Charsets.ISO_8859_1)
                     }
                     "rot" -> {
                         val rotShift = action.rotShift
