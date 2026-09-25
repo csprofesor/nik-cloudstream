65751818988dfd518d3178b1dd3fe9872a810f85
gokturksahan <gokturksahanss@gmail.com>
2026-09-26 01:50:08 +0300

    Fix unresolved iframeDoc reference in loadLinks

--- HDFilmCehennemi/src/main/kotlin/com/keyiflerolsun/HDFilmCehennemi.kt
+++ HDFilmCehennemi/src/main/kotlin/com/keyiflerolsun/HDFilmCehennemi.kt
@@ -370,8 +370,11 @@ override suspend fun loadLinks(
                 referer = data
             ).text
             Log.d("HDCH", "Found videoID: $videoID")
-            var iframe = Regex("""data-src=\\"([^"]+)""").find(apiGet)?.groupValues?.get(1)!!.replace("\\", "")
-            Log.d("HDCH", "$iframe » $iframe")
+            val hdfc = AppUtils.tryParseJson<HDFC>(apiGet)
+            val iframeHtml = hdfc?.html.takeIf { !it.isNullOrEmpty() } ?: apiGet
+            val iframeDoc = Jsoup.parse(iframeHtml)
+            var iframe = fixUrlNull(iframeDoc.selectFirst("iframe")?.attr("data-src") ?: iframeDoc.selectFirst("iframe")?.attr("src")) ?: return@forEach
+
             if (iframe.contains("rapidrame")) {
                 iframe = "${mainUrl}/rplayer/" + iframe.substringAfter("?rapidrame_id=")
             } else if (iframe.contains("mobi")) {
